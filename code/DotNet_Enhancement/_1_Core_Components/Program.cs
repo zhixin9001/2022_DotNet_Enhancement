186b3121058c6d0a091137fe5bf9e327ceb330e6
Zhi Xin <zhi.xin@myob.com>
2022-11-08 08:01:04 +0800

    11.8

---
+++ code/DotNet_Enhancement/_1_Core_Components/Program.cs
@@ -0,0 +1,6 @@
+﻿using _1_Core_Components.configuration;
+using _1_Core_Components.log;
+
+// Configuration.Entry(args);
+
+Log.Entry();
---
+++ code/DotNet_Enhancement/_1_Core_Components/configuration/Configuration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace _1_Core_Components.configuration;
+
+public class Configuration
+{
+    public static void Entry(string[] args)
+    {
+        var config = Json();
+        // var config = CommandLine(args);
+        // var config = EnvVariable();
+
+        Console.WriteLine(config.GetDebugView());
+        
+        var name = config["name"];
+        Console.WriteLine(name);
+
+        var gender = config["info:gender"];
+        Console.WriteLine(gender);
+
+        var gender1 = config.GetSection("info:gender").Value;
+        Console.WriteLine(gender1);
+    }
+
+    public static IConfigurationRoot Json()
+    {
+        var configBuilder = new ConfigurationBuilder();
+        configBuilder.AddJsonFile("./configuration/config.json", optional: false);
+        var config = configBuilder.Build();
+        return config;
+    }
+
+    public static IConfigurationRoot CommandLine(string[] args)
+    {
+        var configBuilder = new ConfigurationBuilder();
+        configBuilder.AddCommandLine(args);
+        return configBuilder.Build();
+    }
+
+    public static IConfigurationRoot EnvVariable()
+    {
+        var configBuilder = new ConfigurationBuilder();
+        configBuilder.AddEnvironmentVariables();
+        return configBuilder.Build();
+    }
+}
